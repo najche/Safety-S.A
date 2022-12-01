2de03041fd9f417761bc6a989cacbed98c0a583d
OscarRoca25 <108959754+Oscar-Roca@users.noreply.github.com>
2022-12-01 11:24:09 -0600

    Include menu to index

--- Safety/index.php
+++ Safety/index.php
@@ -7,77 +7,9 @@
             <link rel="stylesheet" href="styles.css">
     </head> 
     <body>
-    <div class="sidebar" id="sidebar">
-        <span id="btnMenu" class="fa fa-bars menu-icon" data-bs-toggle="collapse" data-bs-target="#menu"></span>
-        <div class="collapse menu-items" id="menu">
-            <span id="companiesDropdown" class="fa fa-building menu-item dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" title="Companies">  Companies</span>
-            <div class="dropdown-menu" aria-labelledby="companiesDropdown">
-                <a class="dropdown-item" href="?tab=search-company"><i class="fa fa-search"></i> Search Company</a>
-                <a class="dropdown-item" href="?tab=add-company"><i class="fa fa-add"></i> Add Company</a>
-            </div>
-
-            <span id="certificatesDropdown" class="fa fa-certificate menu-item dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" title="Certificates">  Certificates</span>
-            <div class="dropdown-menu" aria-labelledby="certificatesDropdown">
-                <a class="dropdown-item" href="?tab=search-certificate"><i class="fa fa-search"></i> Search Certificate</a>
-                <a class="dropdown-item" href="?tab=add-certificate"><i class="fa fa-add"></i> Add Certificate</a>
-            </div>
-
-            <span id="instructorsDropdown" class="fa fa-user menu-item dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" title="Instructors">  Instructors</span>
-            <div class="dropdown-menu" aria-labelledby="instructorsDropdown">
-                <a class="dropdown-item" href="?tab=search-instructor"><i class="fa fa-search"></i> Search Instructor</a>
-                <a class="dropdown-item" href="?tab=add-instructor"><i class="fa fa-add"></i> Add Instructor</a>
-            </div>
-
-            <span id="trainersDropdown" class="fa fa-user menu-item dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" title="Trainers">  Trainers</span>
-            <div class="dropdown-menu" aria-labelledby="trainersDropdown">
-                <a class="dropdown-item" href="?tab=search-trainer"><i class="fa fa-search"></i> Search Trainer</a>
-                <a class="dropdown-item" href="?tab=add-trainer"><i class="fa fa-add"></i> Add Trainer</a>
-            </div>
-
-            <span class="fa fa-power-off menu-item" title="Logout">  Logout</span>
-        </div>
-    </div>
-    <nav id="navbar" class="navbar navbar-customized">
-        <div class="container-fluid">
-            <img class="navbar-logo" src="img/SAFETY_logo_example.png" height="60px"/>
-            <form class="d-flex navbar-search" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-light" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
+    <?php include 'menu.php' ?>
     <div id="content" class="content">
-        <?php 
-            if(array_key_exists('tab', $_GET)){
-                switch($_GET['tab']){
-                    case 'search-company':
-                        break;
-                    case 'add-company':
-                        include 'Company.php';
-                        break;
-                    case 'search-certificate':
-                        break;
-                    case 'add-certificate':
-                        break;
-                    case 'search-instructor':
-                        break;
-                    case 'add-instructor':
-                        include 'employee.php';
-                        break;
-                    case 'search-trainer':
-                        break;
-                    case 'add-trainer':
-                        break;
-                    default:
-                        include 'Company.php';
-                        break;
-                }                
-            }else{
-                include 'Company.php';
-            }
-            
-            
-        ?>
+        <?php include 'Company.php' ?>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.1.slim.js" integrity="sha256-tXm+sa1uzsbFnbXt8GJqsgi2Tw+m4BLGDof6eUPjbtk=" crossorigin="anonymous"></script>
